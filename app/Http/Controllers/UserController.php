682853085d1801227c14c35c049d0a2079a46c40
Kairat Ubukulov <kairat_ubukulov@mail.ru>
2020-01-20 12:38:27 +0600

    fiox

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -63,51 +63,45 @@ class UserController extends BaseController
             'sum' => $sum
         ]);
 
-        DB::beginTransaction();
-        try {
-            if ($payment) {
-                $lastInsertId = $payment->id;
+        if ($payment) {
+            $lastInsertId = $payment->id;
 
-                $pg_merchant_id = 511867;
-                $pg_salt = 'y7crxXTrz6SXKPNd';
+            $pg_merchant_id = 511867;
+            $pg_salt = 'y7crxXTrz6SXKPNd';
 
-                $request = [
-                    'pg_merchant_id'=> $pg_merchant_id,
-                    'pg_amount' => $sum,
-                    'pg_salt' => $pg_salt,
-                    'pg_order_id' => $lastInsertId,
-                    'pg_description' => 'Описание заказа',
-                    'pg_success_url' => 'https://admotionapp.com/payment',
-                    'pg_failure_url' => 'https://admotionapp.com/payment/error',
-                ];
+            $request = [
+                'pg_merchant_id'=> $pg_merchant_id,
+                'pg_amount' => $sum,
+                'pg_salt' => $pg_salt,
+                'pg_order_id' => $lastInsertId,
+                'pg_description' => 'Описание заказа',
+                'pg_success_url' => 'https://admotionapp.com/payment',
+                'pg_failure_url' => 'https://admotionapp.com/payment/error',
+            ];
 
 //        $request['pg_testing_mode'] = 1; //add this parameter to request for testing payments
 
-                //if you pass any of your parameters, which you want to get back after the payment, then add them. For example:
-                $request['client_name'] = (empty(Auth::user()->first_name)) ? 'Admotion' : Auth::user()->first_name;
-                $request['partner_id'] = $partner_id;
-                $request['client_address'] = (empty(Auth::user()->address)) ? 'Earth Planet' : Auth::user()->address;
+            //if you pass any of your parameters, which you want to get back after the payment, then add them. For example:
+            $request['client_name'] = (empty(Auth::user()->first_name)) ? 'Admotion' : Auth::user()->first_name;
+            $request['partner_id'] = $partner_id;
+            $request['client_address'] = (empty(Auth::user()->address)) ? 'Earth Planet' : Auth::user()->address;
 
-                //generate a signature and add it to the array
-                ksort($request); //sort alphabetically
-                array_unshift($request, 'payment.php');
-                array_push($request, $pg_salt); //add your secret key (you can take it in your personal cabinet on paybox system)
+            //generate a signature and add it to the array
+            ksort($request); //sort alphabetically
+            array_unshift($request, 'payment.php');
+            array_push($request, $pg_salt); //add your secret key (you can take it in your personal cabinet on paybox system)
 
 
-                $request['pg_sig'] = md5(implode(';', $request));
+            $request['pg_sig'] = md5(implode(';', $request));
 
-                unset($request[0], $request[1]);
+            unset($request[0], $request[1]);
 
-                $query = http_build_query($request);
-                //redirect a customer to payment page
-                header('Location: https://api.paybox.money/payment.php?'.$query);
-                exit();
-            } else {
-                dd("Ошибка сервера");
-            }
-        }catch (\Exception $exception) {
-            DB::rollBack();
-            dd($exception);
+            $query = http_build_query($request);
+            //redirect a customer to payment page
+            header('Location: https://api.paybox.money/payment.php?'.$query);
+            exit();
+        } else {
+            dd("Ошибка сервера");
         }
     }
 }
