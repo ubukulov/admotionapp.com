6d4b925f27d01b34fa2916a95d15828c144cf4a0
Kairat Ubukulov <kairat_ubukulov@mail.ru>
2020-01-24 18:13:34 +0600

    fux

--- resources/views/pattern/header.blade.php
+++ resources/views/pattern/header.blade.php
@@ -49,8 +49,9 @@
                 @endif
             </ul>
 
+            @if(Auth::check())
             <button type="button" style="margin-right: 40px;" class="btn btn-success" data-toggle="modal" data-target="#quick_pay">Оплатить</button>
-
+            @endif
             <form class="form-inline my-2 my-lg-0">
                 <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
                 <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
