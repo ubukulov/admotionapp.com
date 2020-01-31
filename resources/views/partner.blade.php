6c116c24a2157da9362790752c22669d2fb07206
Kairat Ubukulov <kairat_ubukulov@mail.ru>
2020-01-31 11:25:42 +0600

    fix

--- resources/views/partner.blade.php
+++ resources/views/partner.blade.php
@@ -43,8 +43,6 @@
                 <nav>
                     <div class="nav nav-tabs" id="nav-tab" role="tablist">
                         <a class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">Список призов</a>
-                        <a class="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="false">Profile</a>
-                        <a class="nav-item nav-link" id="nav-contact-tab" data-toggle="tab" href="#nav-contact" role="tab" aria-controls="nav-contact" aria-selected="false">Contact</a>
                     </div>
                 </nav>
                 <div class="tab-content" id="nav-tabContent">
@@ -87,8 +85,6 @@
                             </tbody>
                         </table>
                     </div>
-                    <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">...</div>
-                    <div class="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">...</div>
                 </div>
             </div>
         </div>
