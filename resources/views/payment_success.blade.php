933b9af5e19c3bf6fa82ba007d464fe9372ee1ec
Kairat Ubukulov <kairat_ubukulov@mail.ru>
2020-02-06 16:05:28 +0600

    payment fix

--- resources/views/payment_success.blade.php
+++ resources/views/payment_success.blade.php
@@ -10,45 +10,21 @@
                 $gift_partner = '';
             }
         @endphp
-    <!-- Modal -->
-    <div class="modal fade" id="thank_you" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h4 class="modal-title" id="exampleModalLabel">Поздравляем, оплата прошла успешно!</h4>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body text-center">
-                    <div class="text-center mb-4">
-                        <i class="fas fa-check-circle" style="font-size: 60px; color: green;"></i>
-                    </div>
-                    <p>Поздравляем, {{ $payment->user->first_name }}, Ваша оплата на сумму {{ $payment->sum }}тг прошла успешно!</p>
-                    <p><strong>Вы выиграли приз</strong>: {{ $gift_title }} от <strong>{{ $gift_partner }}</strong>. </p>
-                    <p>Также Вам поступит sms с кодом, которое необходимо предъявить партнёру для получения подарка</p>
 
-
-                    <p><strong>Внимание!</strong></p>
-                    <p>Если оплата не прошла, нажмите эту <a href="{{ route('payment_status', ['id' => $payment->id]) }}" class="btn btn-success">Проверить</a></p>
-
-
-                    <p>Sms код партнёр сравнивает с кодом в кабинете, нажимает кнопку "Вручил" и выдаёт приз</p>
-                </div>
-
-                <div class="modal-footer text-center">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Закрыть</button>
-                </div>
+        <div class="col-md-12">
+            <div class="text-center mb-4">
+                <i class="fas fa-check-circle" style="font-size: 60px; color: green;"></i>
             </div>
-        </div>
-    </div>
+            <p>Поздравляем, {{ $payment->user->first_name }}, Ваша оплата на сумму {{ $payment->sum }}тг прошла успешно!</p>
+            <p><strong>Вы выиграли приз</strong>: {{ $gift_title }} от <strong>{{ $gift_partner }}</strong>. </p>
+            <p>Также Вам поступит sms с кодом, которое необходимо предъявить партнёру для получения подарка</p>
 
-        @php
-        echo "<script type='text/javascript'>
-                setTimeout(function(){
-                    $('#thank_you').modal();
-                }, 2000);
-        </script>";
-        @endphp
+
+            <p><strong>Внимание!</strong></p>
+            <p>Если оплата не прошла, нажмите эту <a href="{{ route('payment_status', ['id' => $payment->id]) }}" class="btn btn-success">Проверить</a></p>
+
+
+            <p>Sms код партнёр сравнивает с кодом в кабинете, нажимает кнопку "Вручил" и выдаёт приз</p>
+        </div>
     @endif
 @stop
