294800d9caf8116fe9d458adba48186520619c0e
Kairat Ubukulov <kairat_ubukulov@mail.ru>
2020-02-06 15:38:14 +0600

    payment fix

--- resources/views/payment_success.blade.php
+++ resources/views/payment_success.blade.php
@@ -11,7 +11,7 @@
             }
         @endphp
     <!-- Modal -->
-    <div class="modal" id="thank_you" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal fade" id="thank_you" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
